aba990ca59de815ab0dc20bd98d6b265f0ca1974
Aleksei Konstantinov <mralexey111@gmail.com>
2024-10-16 11:47:33 +0300

    Fix

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from blogicum import settings
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('pages.urls')),
